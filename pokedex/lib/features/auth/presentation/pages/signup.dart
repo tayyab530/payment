03b161efaaf0a9e33035071ac801b23d86e471f6
tayyabahmed <mmtayyab530@gmail.com>
2023-08-26 22:24:40 +0500

    init

---
+++ pokedex/lib/features/auth/presentation/pages/signup.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_form_builder/flutter_form_builder.dart';
+import 'package:form_builder_validators/form_builder_validators.dart';
+
+import '../blocs/auth.cubit.dart';
+
+class SignupScreen extends StatelessWidget {
+  const SignupScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Signup')),
+      body: BlocProvider(
+        create: (context) => AuthenticationCubit(),
+        child: SignupForm(),
+      ),
+    );
+  }
+}
+
+class SignupForm extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<AuthenticationCubit, AuthenticationState>(
+      builder: (context, state) {
+        final cubit = context.read<AuthenticationCubit>();
+
+        return Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: FormBuilder(
+            child: Column(
+              children: [
+                FormBuilderTextField(
+                  name: 'email',
+                  decoration: const InputDecoration(labelText: 'Email'),
+                  validator: FormBuilderValidators.compose([
+                    FormBuilderValidators.required(),
+                    FormBuilderValidators.email(),
+                  ]),
+                ),
+                FormBuilderTextField(
+                  name: 'password',
+                  decoration: const InputDecoration(labelText: 'Password'),
+                  obscureText: true,
+                  validator: FormBuilderValidators.compose([
+                    FormBuilderValidators.required(),
+                    FormBuilderValidators.minLength(6),
+                  ]),
+                ),
+                const SizedBox(height: 16),
+                ElevatedButton(
+                  onPressed: () {
+                    if (FormBuilder.of(context)!.validate()) {
+                      final email = FormBuilder.of(context)!.value['email'] as String;
+                      final password = FormBuilder.of(context)!.value['password'] as String;
+                      cubit.signup(email, password);
+                    }
+                  },
+                  child: const Text('Signup'),
+                ),
+              ],
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
